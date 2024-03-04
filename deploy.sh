d31a726f13b5a4c8fc91a0e01ef42849d8b234c2
Jan Jongboom <janjongboom@gmail.com>
2024-03-04 10:07:49 +0100

    Update deploy script to use pinned dependencies

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,27 @@ set -e
 
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
+cd $SCRIPTPATH
+
+if [ ! -d ".venv" ]; then
+    if [ -z "$SB_API_KEY" ]; then
+        echo "Missing SB_API_KEY, set to a StableBuild API Key (required to install pinned build dependencies)"
+        exit 1
+    fi
+
+    python3.9 -m venv .venv
+    source .venv/bin/activate
+
+    pip3 install \
+        -i https://$SB_API_KEY.pypimirror.stablebuild.com/2023-01-30/ \
+        twine
+else
+    source .venv/bin/activate
+fi
+
 rm -rf build/
 rm -rf dist/
-python3 setup.py sdist
-twine upload dist/*
+.venv/bin/python3 setup.py sdist
+.venv/bin/twine upload dist/*
+
+deactivate
